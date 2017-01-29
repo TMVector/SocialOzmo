3e649e36120f61b2f161728b95d0170329255f5f
Unknown <tmvector@gmail.com>
2017-01-29 10:14:43 +0000

    Fable Ozmo sample running
    MSBuild/Command-line/Windows-dev is no fun T_T

---
+++ README.md
@@ -0,0 +1,5 @@
+Social Ozmo
+===========
+
+This is fork of [Ozmo](https://github.com/fable-compiler/Fable/tree/master/samples/browser/ozmo)
+which integrates social features.
---
+++ SocialOzmo/Server/Program.fs
@@ -0,0 +1,26 @@
+﻿
+open System.Threading
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+let app =
+    choose
+        [ GET >=> path "/" >=>  Files.file "./public/index.html"
+          GET >=> Files.browseHome
+          RequestErrors.NOT_FOUND "Page not found"
+          POST >=> choose
+                [] ]
+
+
+
+
+let cts = new CancellationTokenSource()
+let conf = { defaultConfig with cancellationToken = cts.Token;
+                                homeFolder = Some (System.IO.Path.GetFullPath "./public") }
+let listening, server = startWebServerAsync conf app
+Async.Start(server, cts.Token)
+printfn "Make requests now"
+System.Console.ReadKey true |> ignore
+cts.Cancel()
